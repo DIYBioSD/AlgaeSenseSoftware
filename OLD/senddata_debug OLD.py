5a24ed023cb4c1f20f607d4f91a983a4468008ff
Collabor8 <seluronking@gmail.com>
2014-04-17 11:34:47 -0700

    Version 2.03
    Added 3 PY files depending on the # of arduinos. Was having problems
    with the Pi crashing.

---
+++ OLD/senddata_debug OLD.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import serial
+from datetime import datetime
+
+def scriptdebug(errormessage):
+    with open("error.log", "a") as errorlog:
+        errorlog.write("%s\t%s\n" % (datetime.strftime(datetime.now(),
+                                     "%d-%m-%y %H:%M"),
+                                     errormessage))
+									 
+
+try:
+	ser = serial.Serial('/dev/ttyACM0', 9600)
+	ser1 = serial.Serial('/dev/ttyACM1', 9600)
+	ser2 = serial.Serial('/dev/ttyACM2', 9600)
+	while 1 :
+		x = ser.readline()
+		y = ser1.readline()
+		z = ser2.readline()
+		if x :
+				print x.strip()
+				output_file = open("/var/www/curdata1.txt", "ab")
+				output_file.write(datetime.strftime(datetime.now(),"%d-%m-%y %H:%M"))
+				output_file.write(x)
+				#output_file.write('\n')
+				output_file.close()
+		if y :
+				print y.strip()
+				output_file = open("/var/www/curdata1.txt", "ab")
+				output_file.write(datetime.strftime(datetime.now(),"%d-%m-%y %H:%M"))
+				output_file.write(y)
+				#output_file.write('\n')
+				output_file.close()		
+		if z :
+				print z.strip()
+				output_file = open("/var/www/curdata1.txt", "ab")
+				output_file.write(datetime.strftime(datetime.now(),"%d-%m-%y %H:%M"))
+				output_file.write(z)
+				#output_file.write('\n')
+				output_file.close()		
+					#code
+except Exception, e:
+	scriptdebug(e)
